70f4c7dcf56dd46be4d11a30268601e4c7942335
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-09-25 09:24:21 +0000

    Added New Files

---
+++ Basics/01_Variables.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+let accountEmail = "Saranshsharma@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+accountEmail = "Saranshsharma20@gmail.com"
+accountPassword = "345666"
+accountCity = "Delhi"
+
+let accountState;
+
+// accountId = 2    Not Allowed 
+
+/* {
+    scope
+    } 
+*/
+
+/*
+   Prefer Not To Use var
+   Becouse Of Issue In Block And Functional Scope
+*/
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ Basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+"Use Strict"; // Treat all JS Code as Newer Version
+
+// alert(3+3) We are Using Node Js not Browser
+
+console.log(3 + 3)
+
+console.log("Saransh")
+
+
+let name = "Saransh"  // String
+let age = 21 // Number Range - 2**53
+let isLoggedIn = true // Boolean
+let state; // Undefined
+let city = null // Null
+
+// Big Int Rnage - More Than 2**53
+// Null (Stand Alone Value)
+// Undefined 
+// Symbol (Basically Used In React) => Unique
+// Object
+
+console.log(typeof "Saransh") // String Type
+console.log(typeof null)  // Object Type
+console.log(typeof undefined) // Undefined Type
