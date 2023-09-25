879c260e8663e91a2690fb88b8d933621032bd7f
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-09-25 10:28:37 +0000

    Operations in JS

---
+++ Basics/04_Operations.js
@@ -0,0 +1,34 @@
+let value = 3
+let negValue = -value
+console.log(value)
+console.log(negValue)
+
+console.log()
+
+console.log(2 + 2)
+console.log(2 - 2)
+console.log(2 * 2)
+console.log(2 / 3)
+console.log(2 % 3)
+console.log(2 ** 3)
+
+let str1 = "Hello"
+let str2 = " Saransh"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+""); // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100
+++gameCounter;
+console.log(++gameCounter);
+
