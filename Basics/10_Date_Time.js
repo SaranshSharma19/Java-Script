b9d36418674c473d31d3772f96eae97caacbdc48
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-09-26 10:54:44 +0000

    Date & Time

---
+++ Basics/10_Date_Time.js
@@ -0,0 +1,12 @@
+/*
+        Dates
+*/
+
+let myDate = new Date()
+console.log(myDate.toString())
+console.log(myDate.getDate())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toTimeString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(typeof myDate)
