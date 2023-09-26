e86b47f3c83bd640f5c2f1b21b9103068f911a84
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-09-26 11:13:21 +0000

    Date & Time

--- Basics/10_Date_Time.js
+++ Basics/10_Date_Time.js
@@ -10,3 +10,27 @@ console.log(myDate.toTimeString())
 console.log(myDate.toDateString())
 console.log(myDate.toLocaleString())
 console.log(typeof myDate)
+
+console.log()
+
+let myCreatedDate = new Date(2023, 1, 23, 5,3)
+let myCreatedDate1= new Date("2023-02-14")
+let myCreatedDate2= new Date("02-19-2002")
+console.log(myCreatedDate2.toLocaleString())
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime())
+console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate.getMonth() + 1)
+console.log(newDate.getDay())
+
+
+newDate.toLocaleString("default",{
+        weekday: "long"
+})
+
+
