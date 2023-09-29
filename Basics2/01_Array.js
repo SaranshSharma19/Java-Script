72d758d915f74d932cc755d11eda5e515f8dc41e
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-09-29 10:04:20 +0000

    Array Methods

--- Basics2/01_Array.js
+++ Basics2/01_Array.js
@@ -1,14 +1,14 @@
 const marvel_heros = ["Bat","Spider","Hulk"]
 const dc_heros = ["Superman","Flash","Berry"]
 
-// marvel_heros.push(dc_heros)
-// console.log(marvel_heros)
+marvel_heros.push(dc_heros)
+console.log(marvel_heros)
 
-// const all_heros = marvel_heros.concat(dc_heros)
-// console.log(all_heros)
+const all_heros = marvel_heros.concat(dc_heros)
+console.log(all_heros)
 
-// const all_new_heros = [...marvel_heros,...dc_heros]
-// console.log(all_new_heros)
+const all_new_heros = [...marvel_heros,...dc_heros]
+console.log(all_new_heros)
 
 
 const anotherArray = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
