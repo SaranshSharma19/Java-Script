014f470a88f795e350bdc3e9fd80f6f90cc5b280
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-09-30 07:17:36 +0000

    Object De-Structuring And JSON API Intro

---
+++ Basics2/04_Objects.js
@@ -0,0 +1,33 @@
+// De-Structuring
+
+const course = {
+    courseName: "Python",
+    price: "1919",
+    courseInstructor:"Saransh Sharma"
+}
+
+const {courseInstructor} = course
+const {courseName: CN} = course
+
+console.log(courseInstructor);
+console.log(CN);
+
+// JSON API Introduction
+
+// {
+//     "name":"Saransh",
+//     "courseNameOne": "Python",
+//     "price": "free"
+// }
+
+// [
+//     {
+
+//     },
+//     {
+
+//     },
+// ]
+
+
+
