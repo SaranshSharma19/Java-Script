66784d0737b1bf7462d78a65f75ac756250fb43f
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-10-10 09:22:28 +0000

    Switch

---
+++ Intermediate/02_Switch.js
@@ -0,0 +1,50 @@
+/*
+    switch(key){
+        case value:
+            break;
+        case value:
+            break;
+        default:
+            break;
+        }
+*/
+
+const month = 3
+switch (month){
+    case 1:
+        console.log("Janurary");
+        break;
+    case 2:
+        console.log("Feburary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("Nob=vember");
+        break;
+    default:
+        console.log("Janurary");
+        break;
+}
