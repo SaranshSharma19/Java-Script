35bee3ca804fdb8a8b1c44f01e8d4e4d9c7c5ba3
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-11-06 14:39:49 +0000

    Comment in JS

---
+++ Basics/14_Comments.js
@@ -0,0 +1,23 @@
+// Single Line Comment
+
+// we use "//" for Single Line Comment
+
+// Multi Line Comment
+
+// We use "/* */" for Multi Line Comment
+
+/*
+    Hi
+    Hello
+    Hey
+    Bye
+    Gn
+*/
+
+// console.log("Saransh")
+console.log("what");
+/*
+console.log("Hello");
+console.log("Hi");
+*/
+console.log("Why");
