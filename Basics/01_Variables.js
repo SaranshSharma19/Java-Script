c169de103f56304a750f768dc66765df29a21aae
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-11-06 14:39:58 +0000

    Variables

--- Basics/01_Variables.js
+++ Basics/01_Variables.js
@@ -1,4 +1,4 @@
-const accountId = 144553
+const accountId = 144553  // We can't change the value of const
 let accountEmail = "Saranshsharma@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
@@ -6,7 +6,26 @@ accountEmail = "Saranshsharma20@gmail.com"
 accountPassword = "345666"
 accountCity = "Delhi"
 
-let accountState;
+// Var
+var x = "Baba"
+var x = "Baba"  // Possible
+x = "Babas"
+console.log(x)
+{
+    var x = "Hello"
+}
+console.log(x)
+
+// Let
+let accountState = "GZB";
+//  let accountState = "GZB"; // Not Possible
+accountState = "Del"
+console.log(accountState)
+{
+    let accountState = "Hello"
+    console.log(accountState)
+}
+console.log(accountState)
 
 // accountId = 2    Not Allowed 
 
