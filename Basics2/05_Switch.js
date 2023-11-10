92f3e80dd5a3fa21578e70e877495a3ade0c8d69
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-11-10 10:55:55 +0000

    Switch

---
+++ Basics2/05_Switch.js
@@ -0,0 +1,11 @@
+/*
+    switch(expression){
+        case condition 1: statement(s)
+        break;
+        case condition 2: statement(s)
+        break;
+        case condition 3: statement(s)
+        break;
+        default:statement(s)
+    }
+*/
