a7817f116416c1409874047343f762f091fdea49
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-11-10 10:50:31 +0000

    Ternary Operator

---
+++ Basics/20_TernaryOperator.js
@@ -0,0 +1,14 @@
+// (Condition) ? True Statement : False Statement
+var x = 15;
+var z;
+
+(x>10) ? z=true : z=false
+console.log(z)
+
+x = 100;
+(x<90) ? z = true : z = false
+console.log(z)
+
+x = 100;
+z = "Value is " + ((x<90) ? "true" : "false")
+console.log(z)
