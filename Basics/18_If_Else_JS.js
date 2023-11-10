c23cdd2534a4e1dd5c3256af1ad2b6818e6c4d00
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-11-10 09:44:03 +0000

    If Else in JS

---
+++ Basics/18_If_Else_JS.js
@@ -0,0 +1,25 @@
+var x = 15;
+if (x > 30){
+    console.log("X Is Greater Than 30")
+}
+else{
+    console.log("X Is Smaller Than 30")
+}
+
+var x = "100";
+
+if (x==100){
+    console.log("Same in  ==")
+}
+else{
+    console.log("Not Same in ==")
+}
+
+var x = "100";
+
+if (x===100){
+    console.log("Same in  ===")
+}
+else{
+    console.log("Not Same in ===")
+}
