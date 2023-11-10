304f43551334d06d0bc50d27d9e8e842f9851884
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-11-10 09:50:12 +0000

    If Else If in JS

---
+++ Basics/19_If_Else_If.js
@@ -0,0 +1,10 @@
+var marks = 20
+if (marks < 18){
+    console.log("You Failed The Exam")
+}
+else if(marks == 18){
+    console.log("You Got B Grade")
+}
+else{
+    console.log("You Got A Grade")
+}
