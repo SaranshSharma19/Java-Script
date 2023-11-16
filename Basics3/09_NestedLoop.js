e211c75c75b06ac1acf351694bcae2f3e8009b6c
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-11-16 09:19:22 +0000

    Nested Loop

--- Basics3/09_NestedLoop.js
+++ Basics3/09_NestedLoop.js
@@ -4,3 +4,32 @@ for(var a = 1; a <= 100; a = a + 10){
     }
     console.log("\n")
 }
+
+
+for(var a = 1; a <= 5; a = a + 1){
+    for(var b = 1; b <= a; b++){
+        console.log(b + "");
+    }
+    console.log("\n")
+}
+
+for(var a = 1; a <= 5; a = a + 1){
+    for(var b = 1; b <= a; b++){
+        console.log(a + "");
+    }
+    console.log("\n")
+}
+
+for(var a = 5; a >= 1; a = a - 1){
+    for(var b = a; b >= 1; b--){
+        console.log(a + "");
+    }
+    console.log("\n")
+}
+
+for(var a = 5; a >= 1; a = a - 1){
+    for(var b = a; b >= 1; b--){
+        console.log(b + "");
+    }
+    console.log("\n")
+}
