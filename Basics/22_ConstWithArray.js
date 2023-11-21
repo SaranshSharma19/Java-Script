3734a813c1574165998ad3bd3bac5a5342ed3aad
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-11-21 13:59:12 +0000

    Const Wth Array & Object

---
+++ Basics/22_ConstWithArray.js
@@ -0,0 +1,23 @@
+const a =[10,20,30]
+console.log(a)
+a[1] = 25  // Valid
+console.log(a)
+
+// a = [40,50,60] // Not valid
+// console.log(a)
+
+
+const b = {
+    name: "Ram",
+    age: 25
+}
+console.log(b)
+b["name"] = "Saransh"  // valid
+console.log(b)
+
+// b = {
+//     name: "Shyam",
+//     age: 50
+// }
+// Not Valid
+// console.log(b)
