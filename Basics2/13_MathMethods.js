13adc2653bda23acc46b05a5a8b55e64361bf83e
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-11-22 16:28:59 +0000

    Math Methods

---
+++ Basics2/13_MathMethods.js
@@ -0,0 +1,36 @@
+// ceil
+console.log("Ceil: ",Math.ceil(5.7))
+
+// floor
+console.log("Floor: ",Math.floor(5.7))
+
+// round
+console.log("Round: ", Math.round(5.6))
+
+// trunc
+console.log("Trunc: ", Math.trunc(5.6))
+
+// Max
+console.log("Max: ",Math.max(10,20,30,40))
+
+// Min
+console.log("Min: ",Math.min(10,20,30,40))
+
+// sqrt
+console.log("SQRT: ",Math.sqrt(4))
+
+// cbrt
+console.log("CBRT: ",Math.cbrt(8))
+
+// pow
+console.log("POW: ",Math.pow(2,3))
+
+// random
+console.log("Random: ",Math.random())
+console.log("Random 1: ",Math.floor(Math.random()*10) + 1)
+
+// abs
+console.log("ABS: ",Math.abs(-78.999))
+
+// Pi
+console.log("PI: ",Math.PI)
