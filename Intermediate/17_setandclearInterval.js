7d6c9dd1b9863b38c3d9adfc93befd33f6a7e600
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-11-30 11:37:42 +0000

    setInterval & clearInterval

---
+++ Intermediate/17_setandclearInterval.js
@@ -0,0 +1,14 @@
+var a = 0
+var id = setInterval(Anim,1000);
+
+function Anim(){
+    a = a + 10
+
+    if (a==200){
+        clearInterval(id);
+    }
+    else{
+        var target = document.getElementById("test")
+        target.style.marginLeft = a + "px";
+    }
+}
