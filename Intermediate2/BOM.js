af3b7db4ec902d4edeeae0af8b09d424e661b42c
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-11-30 12:50:16 +0000

    BOM Introduction

---
+++ Intermediate2/BOM.js
@@ -0,0 +1,15 @@
+/*
+
+    BOM - Browser Object Model - Window
+
+    [Chrome, FireFox, Opera, Internet Explorer, Edge]
+
+    Use of BOM -
+        1 - Get Width & Height of Browser Window
+        2 - Open & Close Browser Window
+        3 - Move & Resize Browser Window
+        4 - Scroll to Browser Window
+        5 - Get URL, Hostname, Protocol of Browser Window
+        6 - Get History of Browser Window
+
+*/
