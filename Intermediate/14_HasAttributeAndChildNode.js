1c1595739ebc7a9099438200d4413fa1b5ae20d1
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-11-30 09:05:42 +0000

    hasChildNode & hasAttribute

---
+++ Intermediate/14_HasAttributeAndChildNode.js
@@ -0,0 +1,9 @@
+var target = document.getElementById("test")
+var find = target.hasAttribute("class")
+console.log(find)
+
+var find = target.hasAttribute("style")
+console.log(find)
+
+var find = target.hasChildNodes()
+console.log(find)
