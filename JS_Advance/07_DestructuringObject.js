2722c69c9e54be8dba890121f51f7d2cfdeb93b5
Saransh Sharma <111983166+SaranshSharma19@users.noreply.github.com>
2023-12-06 14:31:45 +0000

    Destructuring Object

---
+++ JS_Advance/07_DestructuringObject.js
@@ -0,0 +1,19 @@
+var user = {
+    name : "Saransh Sharma",
+    age : 21,
+    city : "Ghaziabad"
+}
+var {name, age, city} = user;
+console.log(name)
+console.log(age)
+console.log(city)
+
+var user = {
+    name : "Saransh Sharma",
+    age : 21,
+    city : "Ghaziabad"
+}
+var {name : n, age : a, city : c} = user;
+console.log(n)
+console.log(a) 
+console.log(c)
